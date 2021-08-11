ec44eb8f162987a9bf517c682322a7084df7a01f
Jinwook <gjwlsdnr0115@gmail.com>
2021-08-11 18:08:24 +0900

    feat: Add search UI

--- LastDay/LastDay/ViewController/HomeViewController.swift
+++ LastDay/LastDay/ViewController/HomeViewController.swift
@@ -48,6 +48,19 @@ class HomeViewController: UIViewController {
     return label
   }()
   
+  lazy var searchButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("  어디로 여행가세요?", for: .normal)
+    button.setTitleColor(UIColor.gray, for: .normal)
+    button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14.0)
+    button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+    button.backgroundColor = UIColor.white
+    button.tintColor = UIColor(displayP3Red: 175/255.0, green: 170/255.0, blue: 226/255.0, alpha: 1)
+    button.addTarget(self, action: #selector(goSearch), for: .touchUpInside)
+    return button
+  }()
+  
   
   
   
@@ -58,7 +71,21 @@ class HomeViewController: UIViewController {
     // Do any additional setup after loading the view.
     addViews()
     setConstraints()
+    setAppearances()
   }
+  
+  // MARK: - Target Functions
+  
+  @objc func goSearch() {
+    let sb = UIStoryboard.init(name: "Search", bundle: nil)
+    let vc = sb.instantiateViewController(identifier: "SearchViewController") as! SearchViewController
+    
+    vc.modalTransitionStyle = .crossDissolve
+    vc.modalPresentationStyle = .fullScreen
+    present(vc, animated: true, completion: nil)
+  }
+  
+  
   
   
   
@@ -69,11 +96,28 @@ class HomeViewController: UIViewController {
     view.addSubview(lastColorMessageLabel)
     view.addSubview(largeGreetingMessageLabel)
     view.addSubview(secondGreetingMessageLabel)
+    
+    view.addSubview(searchButton)
 
   }
   
+  func setAppearances() {
+    
+    // MARK: - Search Button
+    searchButton.layer.cornerRadius = 20
+    
+    searchButton.layer.borderWidth = 1
+    searchButton.layer.borderColor = UIColor(displayP3Red: 175/255.0, green: 170/255.0, blue: 226/255.0, alpha: 1).cgColor
+    
+    searchButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+    searchButton.layer.shadowOpacity = 0.3
+    searchButton.layer.shadowRadius = 2
+    searchButton.layer.shadowColor = UIColor(displayP3Red: 175/255.0, green: 170/255.0, blue: 226/255.0, alpha: 1).cgColor
+  }
   
   func setConstraints() {
+    
+    // MARK: - Greeting Messages
     greetingMessageLabel.snp.makeConstraints { make in
       make.top.equalToSuperview().offset(80)
       make.leading.equalToSuperview().offset(20)
@@ -94,6 +138,13 @@ class HomeViewController: UIViewController {
       make.leading.equalTo(lastColorMessageLabel)
     }
     
+    
+    
+    searchButton.snp.makeConstraints { make in
+      make.top.equalTo(secondGreetingMessageLabel.snp.bottom).offset(40)
+      make.leading.trailing.equalToSuperview().inset(30)
+      make.height.equalTo(40)
+    }
   }
   
 }
---
+++ LastDay/LastDay/ViewController/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  LastDay
+//
+//  Created by Jinwook Huh on 2021/08/11.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
