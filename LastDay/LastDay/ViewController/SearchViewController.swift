a601b457f6adfc0520a0889f05195e0640b318f3
Jinwook <gjwlsdnr0115@gmail.com>
2021-08-14 01:05:03 +0900

    feat: Add buttons and animations

--- LastDay/LastDay/ViewController/SearchViewController.swift
+++ LastDay/LastDay/ViewController/SearchViewController.swift
@@ -19,6 +19,27 @@ class SearchViewController: UIViewController {
     return imgView
   }()
   
+  lazy var firstBackgroundLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "출발 장소와"
+    label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 28.0)
+    label.textColor = UIColor.white
+    label.alpha = 0.0
+    return label
+  }()
+  
+  lazy var secondBackgroundLabel: UILabel = {
+    let label = UILabel()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "시간을 선택하세요."
+    label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 28.0)
+    label.textColor = UIColor.white
+    label.alpha = 0.0
+    return label
+  }()
+  
+  
   lazy var reactingBGView: UIView = {
     let view = UIView()
     view.backgroundColor = UIColor.white
@@ -65,6 +86,8 @@ class SearchViewController: UIViewController {
     stackView.alignment = .center
     stackView.axis = .horizontal
     stackView.spacing = 0
+    stackView.alpha = 0.0
+    stackView.isHidden = true
     
     return stackView
   }()
@@ -105,8 +128,24 @@ class SearchViewController: UIViewController {
     let view = UIView()
     view.layer.borderWidth = 2
     view.layer.borderColor = UIColor(displayP3Red: 175/255.0, green: 170/255.0, blue: 226/255.0, alpha: 1).cgColor
+    view.alpha = 0.0
+    view.isHidden = true
     return view
   }()
+  
+  
+  lazy var nextButton: UIButton = {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("다음", for: .normal)
+    button.setTitleColor(UIColor.white, for: .normal)
+    button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 16.0)
+    button.backgroundColor = UIColor(displayP3Red: 175/255.0, green: 170/255.0, blue: 226/255.0, alpha: 1)
+    button.alpha = 0.0
+    button.isHidden = true
+    return button
+  }()
+  
   
   // MARK: - Variables
   
@@ -137,9 +176,67 @@ class SearchViewController: UIViewController {
     dismiss(animated: true, completion: nil)
   }
   
+  
+  
+  
+  func showTimeSelection() {
+    
+    reactingBGView.snp.remakeConstraints { make in
+      make.height.equalTo(380)
+      make.bottom.leading.trailing.equalToSuperview()
+    }
+    
+    UIView.animate(withDuration: 0.3) {
+      self.timePickerStackView.isHidden = false
+      self.timePickerStackView.alpha = 1.0
+      self.timeWrapView.isHidden = false
+      self.timeWrapView.alpha = 1.0
+      self.nextButton.isHidden = false
+      self.nextButton.alpha = 1.0
+
+      self.view.layoutIfNeeded()
+    } completion: { done in
+      UIView.animate(withDuration: 0.3) {
+        self.firstBackgroundLabel.alpha = 1.0
+        self.secondBackgroundLabel.alpha = 1.0
+        self.view.layoutIfNeeded()
+      }
+    }
+  }
+  
+  func hideTimeSelection() {
+
+    reactingBGView.snp.remakeConstraints { make in
+      make.top.equalToSuperview().offset(50)
+      make.bottom.leading.trailing.equalToSuperview()
+    }
+    
+    
+    UIView.animate(withDuration: 0.3) {
+      self.timePickerStackView.alpha = 0.0
+      self.timeWrapView.alpha = 0.0
+      self.nextButton.alpha = 0.0
+      self.firstBackgroundLabel.alpha = 0.0
+      self.secondBackgroundLabel.alpha = 0.0
+      self.view.layoutIfNeeded()
+    } completion: { done in
+      self.timePickerStackView.isHidden = true
+      self.timeWrapView.isHidden = true
+      self.nextButton.isHidden = true
+
+    }
+    
+
+  }
+  
+  
+  
   // MARK: - Delegates
   
   func setDelegates() {
+    
+    searchTextField.delegate = self
+    
     hourPickerView.delegate = self
     hourPickerView.dataSource = self
     minPickerView.delegate = self
@@ -154,16 +251,20 @@ class SearchViewController: UIViewController {
     view.addSubview(backgroundImgView)
     view.addSubview(cornerBGView)
     view.addSubview(reactingBGView)
+    view.addSubview(firstBackgroundLabel)
+    view.addSubview(secondBackgroundLabel)
     
     // reactingBGView
     reactingBGView.addSubview(backButton)
     reactingBGView.addSubview(searchTextField)
     reactingBGView.addSubview(timePickerStackView)
     reactingBGView.addSubview(timeWrapView)
+    reactingBGView.addSubview(nextButton)
+    
     
     // View order
     view.bringSubviewToFront(reactingBGView)
-    reactingBGView.bringSubviewToFront(timeWrapView)
+    reactingBGView.bringSubviewToFront(timePickerStackView)
   }
   
   
@@ -172,6 +273,8 @@ class SearchViewController: UIViewController {
     
     reactingBGView.layer.cornerRadius = 25
     timeWrapView.layer.cornerRadius = 15
+    nextButton.layer.cornerRadius = 10
+    
     
     
   }
@@ -181,13 +284,23 @@ class SearchViewController: UIViewController {
       make.edges.equalToSuperview()
     }
     
+    secondBackgroundLabel.snp.makeConstraints { make in
+      make.leading.equalToSuperview().offset(20)
+      make.bottom.equalTo(reactingBGView.snp.top).offset(-20)
+    }
+    
+    firstBackgroundLabel.snp.makeConstraints { make in
+      make.leading.equalTo(secondBackgroundLabel)
+      make.bottom.equalTo(secondBackgroundLabel.snp.top)
+    }
+    
     reactingBGView.snp.makeConstraints { make in
       make.top.equalToSuperview().offset(50)
       make.bottom.leading.trailing.equalToSuperview()
     }
     
     cornerBGView.snp.makeConstraints { make in
-      make.height.equalTo(view.bounds.height/2)
+      make.height.equalTo(100)
       make.bottom.leading.trailing.equalToSuperview()
     }
     
@@ -205,15 +318,15 @@ class SearchViewController: UIViewController {
     }
     
     hourPickerView.snp.makeConstraints { make in
-      make.width.equalTo(65)
+      make.width.equalTo(60)
     }
     
     minPickerView.snp.makeConstraints { make in
-      make.width.equalTo(65)
+      make.width.equalTo(60)
     }
     
     ampmPickerView.snp.makeConstraints { make in
-      make.width.equalTo(60)
+      make.width.equalTo(50)
     }
     
     timePickerStackView.addArrangedSubview(ampmPickerView)
@@ -222,7 +335,7 @@ class SearchViewController: UIViewController {
     timePickerStackView.addArrangedSubview(minPickerView)
     timePickerStackView.snp.makeConstraints { make in
       make.centerX.equalToSuperview()
-      make.top.equalTo(searchTextField.snp.bottom).offset(40)
+      make.top.equalTo(searchTextField.snp.bottom).offset(20)
     }
     
     timeWrapView.snp.makeConstraints { make in
@@ -231,12 +344,32 @@ class SearchViewController: UIViewController {
       make.width.equalTo(220)
     }
     
-    
+    nextButton.snp.makeConstraints { make in
+      make.width.equalTo(85)
+      make.height.equalTo(45)
+      make.top.equalTo(timePickerStackView.snp.bottom).offset(20)
+      make.trailing.equalToSuperview().inset(20)
+    }
     
     
   }
   
 }
+
+
+extension SearchViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    showTimeSelection()
+    return true
+  }
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    hideTimeSelection()
+  }
+}
+
 
 extension SearchViewController: UIPickerViewDelegate {
 //  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -276,14 +409,14 @@ extension SearchViewController: UIPickerViewDelegate {
       return view
       
     } else {
-      let view = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 60))
-      let label = UILabel(frame: CGRect(x: 0, y: 0, width: 80, height: 60))
+      let view = UIView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+      let label = UILabel(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
       if pickerView.tag == 0 {
         label.text = hours[row]
-        label.font = UIFont.systemFont(ofSize: 42, weight: .thin)
+        label.font = UIFont.systemFont(ofSize: 34, weight: .thin)
       } else {
         label.text = mins[row]
-        label.font = UIFont.systemFont(ofSize: 42, weight: .thin)
+        label.font = UIFont.systemFont(ofSize: 34, weight: .thin)
       }
       label.textAlignment = .center
       view.addSubview(label)
